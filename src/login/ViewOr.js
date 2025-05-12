cdb51a2c191512a3d7f1a136b596822759069e86
zyyx-linhnh <linhnh@zyyx.jp>
2025-05-12 16:14:32 +0700

    create view basic

---
+++ src/login/ViewOr.js
@@ -0,0 +1,22 @@
+export const ViewOr = () => {
+    return (
+        <div style={{
+            display: 'flex',
+            flex: '1',
+            width: '404px',
+            marginTop: '39px',
+            marginBottom: '50px',
+            alignItems: "center"
+        }}>
+            <div style={{ flex: '1', height: '2px', backgroundColor: '#F5F5F5' }} />
+            <p style={{
+                fontSize: '9px',
+                fontWeight: 'normal',
+                color: 'black',
+                marginLeft: '5px',
+                marginRight: '5px'
+            }}>Or</p>
+            <div style={{ flex: '1', height: '2px', backgroundColor: '#F5F5F5' }} />
+        </div>
+    );
+}
